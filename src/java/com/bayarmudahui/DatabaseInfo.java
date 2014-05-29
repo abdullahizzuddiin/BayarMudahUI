8e29ddc227fb7f0c373c6526e03cce71d7ea7269
abdullahizzuddiin@gmail.com <abdullahizzuddiin@gmail.com>
2014-05-29 09:13:36 +0700

    awal lagi

--- src/java/com/bayarmudahui/DatabaseInfo.java
+++ src/java/com/bayarmudahui/DatabaseInfo.java
@@ -6,10 +6,43 @@
 
 package com.bayarmudahui;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  *
  * @author Abdullah Izzuddiin A
  */
 public class DatabaseInfo {
+    public static final String KELAS = "com.mysql.jdbc.Driver";
+    public static final String URL = "jdbc:mysql://localhost/bayarmudahui"; 
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = "";
+    private Connection conn = null;
+    private Statement stmt = null;
     
+    private void openConnection()
+    {
+        try {
+            Class.forName(KELAS);
+            conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            stmt = conn.createStatement();
+        } catch (Exception e) {            
+        }
+    }
+    
+    private void closeConnection() throws SQLException
+    {
+        if(stmt != null) {
+            try {
+                stmt.close();;
+            } catch (Exception e) {
+            }
+        }
+        if(conn != null) {
+            conn.close();
+        }
+    }
 }
